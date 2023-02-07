0f12b7172131ff9ee5242179eebf23bf6fa99567
PierreAND <andrieu@gmail.com>
2023-02-07 12:07:53 +0100

    fix title

--- src/Containers/Articles/Article.jsx
+++ src/Containers/Articles/Article.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 
-function Article() {
+function Article(props) {
   const location = useLocation()
   console.log(location);
 
   return (
     <div className="article-content">
-      <h1>{location.pathname.replace(/\-/g, " ")}</h1>
     <p>{location.state}</p>
     </div>
   );
