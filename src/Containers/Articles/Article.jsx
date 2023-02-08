d9d4e9bbf513ee48d5e15fc60e418883e3d3c942
PierreAND <andrieu@gmail.com>
2023-02-08 13:32:35 +0100

    Revert "fix title"
    This reverts commit 0f12b7172131ff9ee5242179eebf23bf6fa99567.

--- src/Containers/Articles/Article.jsx
+++ src/Containers/Articles/Article.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 
-function Article(props) {
+function Article() {
   const location = useLocation()
   console.log(location);
 
   return (
     <div className="article-content">
+      <h1>{location.pathname.replace(/\-/g, " ")}</h1>
     <p>{location.state}</p>
     </div>
   );
