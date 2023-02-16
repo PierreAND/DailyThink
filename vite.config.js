6e326581563123203655679ec7bc7b69e9c8d338
PierreAND <andrieu@gmail.com>
2023-02-16 10:55:06 +0100

    pwa new prod

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,23 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({ registerType: 'autoUpdate' })
+    VitePWA({ 
+      manifest: {
+        icons: [
+            {
+              "src": "/public/maskable_icon.png",
+              "sizes": "196x196",
+              "type": "image/png",
+              "purpose": "any maskable"
+            },
+            {
+              "src": "/public/logo192.png",
+              "type": "image/png",
+              "sizes": "192x192"
+            },
+        ]
+      }
+     })
   
   
   ]
